dd2924caa03c03999ee95c5bc15fa85854987287
Yuri Schimke <yschimke@fb.com>
2018-07-04 19:04:47 +0100

    initial import

---
+++ src/main/kotlin/com/baulsupp/sonarokhttp/SonarOkhttpApplication.kt
@@ -0,0 +1,11 @@
+package com.baulsupp.sonarokhttp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SonarOkhttpApplication
+
+fun main(args: Array<String>) {
+    runApplication<SonarOkhttpApplication>(*args)
+}
